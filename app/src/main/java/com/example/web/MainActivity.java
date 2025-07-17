8fe97827dee179afb9e89df6a8419f26395f4860
MohammadAbdulSamad1 <waqas.erio@gmail.com>
2025-07-18 04:56:33 +0500

    Android App for UoPDCS

---
+++ app/src/main/java/com/example/web/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.web;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWeb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        myWeb = findViewById(R.id.myWeb);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+        myWeb.setWebViewClient(new WebViewClient());
+        myWeb.loadUrl("https://www.uopdcs.com/");
+    }
+}
